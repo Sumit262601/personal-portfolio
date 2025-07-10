d6c85ac0b34b5f355bd9f91a839f8981eac852c1
Sumit262601 <skr.04088s@gmail.com>
2025-07-10 13:15:55 +0530

    Add

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -12,16 +12,18 @@ export default function Home() {
       <Heading className="">Hello there! I&apos;m Sumit</Heading>
       <Paragraph className="max-w-4xl mt-4">
         I&apos;m a <Highlight>frontend developer</Highlight> who’s passionate about building clean, responsive, and impactful web applications that elevate user experience. With a strong foundation in the <Highlight>MERN stack,</Highlight> I love transforming ideas into modern, performance-driven products that solve real-world problems.
-
-        I&apos;ve worked on a wide range of projects — from cloud-based note-taking apps and blog management systems to branding websites and Python-based tools like a YouTube video downloader. Whether it&apos;s implementing authentication, integrating rich text editors, managing complex UI state, or handling image uploads, I enjoy tackling challenges with code and creativity.
-
-        I specialize in ReactJS, TailwindCSS, Node.js, Express, and MongoDB, and I&apos;m always experimenting with tools like Docker, Clerk, Redux, and Google OAuth to stay ahead. Beyond just writing code, I focus on building reusable components, pixel-perfect UIs, and scalable architecture.
-
-        I thrive in environments where I can continuously learn, push my limits, and contribute to meaningful digital experiences.
       </Paragraph>
-      
       <Paragraph className="max-w-4xl mt-4">
-        I&apos;m a fresher software engineer building scalable web apps
+        I&apos;ve worked on a wide range of projects — from cloud-based note-taking apps and blog management systems to branding websites and Python-based tools like a YouTube video downloader. Whether it&apos;s implementing authentication, integrating rich text editors or handling image uploads. I enjoy tackling challenges with code and creativity.
+      </Paragraph>
+      <Paragraph className="max-w-4xl mt-4">
+        I specialize in ReactJS, TailwindCSS, Node.js, Express, and MongoDB, and I&apos;m always experimenting with tools like Clerk, Redux, and Google OAuth to stay ahead. Beyond just writing code, I focus on building reusable components, pixel-perfect UIs, and scalable architecture.
+
+        I thrive in environments where I can continuously learn, push my limits, and contribute to meaningful user experiences.
+      </Paragraph>
+
+      <Paragraph className="max-w-4xl mt-4">
+        I&apos;m a fresher frontend developer building scalable web apps
         that are performance optimized and good looking.
       </Paragraph>
       <Heading
